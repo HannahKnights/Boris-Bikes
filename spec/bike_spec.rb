cf897f570f275ab8631a0d3cb78b97544829a287
Hannah Knights <hannah.knights@hotmail.co.uk>
2013-11-26 13:34:26 +0000

    Adding more options for broken bikes

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -1,10 +1,23 @@
 require_relative "../lib/bike"
 
-describe Bike do 
 
-  it "should not be broken when we create it" do
-    bike = Bike.new
-    expect(bike).not_to be_broken
+describe Bike   do 
+
+let(:bike) {Bike.new}
+
+  it "should not be broken after we create it" do
+    expect(bike.broken?).to be_false
+  end
+
+  it "should be able to break" do
+    bike.break
+    expect(bike.broken?).to be_true
+  end
+
+  it "should be able to get fixed" do
+    bike.break
+    bike.fix
+    expect(bike.broken?).to be_false
   end
 
 end
